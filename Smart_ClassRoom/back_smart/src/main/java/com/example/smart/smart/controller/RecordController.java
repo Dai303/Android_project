dfdcd1d95634571e0c4e5824d43f5e879a0041e0
Dai303 <1471324635@qq.com>
2021-06-04 09:19:32 +0800

    [ada]adasd

--- Smart_ClassRoom/back_smart/src/main/java/com/example/smart/smart/controller/RecordController.java
+++ Smart_ClassRoom/back_smart/src/main/java/com/example/smart/smart/controller/RecordController.java
@@ -1,4 +1,51 @@
 package com.example.smart.smart.controller;
 
+import com.example.smart.smart.entity.Record;
+import com.example.smart.smart.service.RecordService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@RestController
+@RequestMapping("/record")
 public class RecordController {
+    @Autowired
+    RecordService recordService;
+
+    @GetMapping("/findALL")
+    public List<Record> getdata(){
+        return recordService.findALL();
+    }
+
+    @GetMapping("/delete")
+    public List<Record> deletedata(@RequestParam String id){
+        recordService.delete(id);
+        return recordService.findALL();
+    }
+
+    @GetMapping("/add")
+    public List<Record> add(@RequestParam String name){
+        Record record = new Record();
+        record.setNumID(name);
+        record.setStartT(LocalDateTime.of(2021,12,12,10,11,12));
+        record.setEndT(LocalDateTime.of(2021,12,12,10,11,12));
+        recordService.insert(record);
+        return recordService.findALL();
+    }
+
+    @GetMapping("/update")
+    public List<Record> update(@RequestParam String name,@RequestParam Integer id){
+        Record record = new Record();
+        record.setNumID(name);
+        record.setStartT(LocalDateTime.of(2021,12,12,10,11,12));
+        record.setEndT(LocalDateTime.of(2021,12,12,10,11,12));
+        record.setId(id);
+        recordService.update(record);
+        return recordService.findALL();
+    }
 }
--- Smart_ClassRoom/back_smart/src/main/java/com/example/smart/smart/service/RecordService.java
+++ Smart_ClassRoom/back_smart/src/main/java/com/example/smart/smart/service/RecordService.java
@@ -1,4 +1,29 @@
 package com.example.smart.smart.service;
 
-public class RecardService {
+import com.example.smart.smart.dao.RecordDao;
+import com.example.smart.smart.entity.Record;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class RecordService {
+    @Autowired
+    RecordService recordService;
+    @Autowired
+    RecordDao recordDao;
+
+    public List<Record> findALL(){
+        return recordDao.findALL();
+    }
+    public void delete(String id){
+        recordDao.delete(id);
+    }
+    public void insert(Record record){
+        recordDao.insert(record);
+    }
+    public void update(Record record){
+        recordDao.update(record);
+    }
 }
